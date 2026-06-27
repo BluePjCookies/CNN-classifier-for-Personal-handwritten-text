474c5c05253124b3fc0de7a713dc931c0fb6e481
BluePjCookies <joshualeong2359@gmail.com>
2026-06-27 23:10:47 +0800

    rm personal files

--- setup.py
+++ setup.py
@@ -6,5 +6,3 @@ import kagglehub
 path = kagglehub.dataset_download("crawford/emnist")
 
 print("Path to dataset files:", path)
-
-# Path to dataset files: /Users/Joshua/.cache/kagglehub/datasets/crawford/emnist/versions/3
